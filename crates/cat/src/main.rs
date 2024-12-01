b4db411222e0328848feea4ead277d2dedb89bbe
Charlotte Thomas <me@charlotte-thomas.me>
2024-12-01 09:57:56 +0100

    add reverse ability

--- crates/cat/src/main.rs
+++ crates/cat/src/main.rs
@@ -6,14 +6,18 @@ use std::process::exit;
 
 fn main() {
     let mut args: Args = env::args();
+    let mut reversed = false;
     if args.len() < 2 {
         println!("Usage: cat <file>");
         exit(1);
     }
+    if args.len() > 2 {
+        reversed = !reversed;
+    }
     let file = args.nth(1);
     match file {
         Some(name) => {
-            let s = cat(name);
+            let s = cat(name,reversed);
             if s.is_err() {
                 println!("Error while reading file");
             }
@@ -22,8 +26,10 @@ fn main() {
     };
 }
 
-fn cat(file: String) -> Result<(), Box<dyn Error>> {
+fn cat(file: String,reversed: bool) -> Result<(), Box<dyn Error>> {
     let content = read_to_string(file)?;
-    println!("{content}");
+    println!("{}",
+        if reversed {content.lines().rev().collect::<Vec<&str>>().join("\n")} else {content}
+    );
     Ok(())
 }
